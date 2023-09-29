556ebc5861ec94bd7deb94ea1b4cce4745b86a3f
Clément CARDOT <clementcdt25@gmail.com>
2023-09-29 13:30:15 +0200

    add Build script

--- Build_LOCAL.sh
+++ Build_LOCAL.sh
@@ -1,28 +1,26 @@
-# This Script is used to build the project and run it in a docker container.
+# # This Script is used to build the project and run it in a docker container.
 
 echo "Step 0 --> Clean up"
-rm -rf back/src/main/resources/static/*
+# rm -rf back/src/main/resources/static/*
 rm -rf back/target/*
 rm -rf taf.war
-rm -rf front/dist/front/*
+# rm -rf front/dist/front/*
 
-# 1. Build Front-end
+# # 1. Build Front-end
 echo "Step 1 --> Build Front-end"
 cd front
 
-# Compile
-npm install --legacy-peer-deps
-ng build --configuration=local --base-href "."
+# # Compile
+# npm install --legacy-peer-deps
+npm install
+ng build --base-href "."
 
-# export to back folder
-cp -r dist/front/* ../back/src/main/resources/static/
-
-# 2. Build Back-end
+# # 2. Build Back-end
 echo "Step 2 --> Build Back-end"
 cd ../back
 
-# Compile back end
+# # Compile back end
 mvn clean package
 
-# put taf.war in main folder
-cp target/taf.war ../
+docker compose build
+docker compose up
